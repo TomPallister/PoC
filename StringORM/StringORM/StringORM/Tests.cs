13a255babbd9298fa46c2eaf94e22c2c4d82df84
TomPallister <tom.pallister@fourth.com>
2014-05-28 16:56:15 +0100

    reformat

--- StringORM/StringORM/StringORM/Tests.cs
+++ StringORM/StringORM/StringORM/Tests.cs
@@ -2,7 +2,6 @@
 using System.Data;
 using System.Data.SqlClient;
 using FluentAssertions;
-using StringORM.Infrastructure;
 using StringORM.Infrastructure.Enums;
 using Xunit;
 
@@ -11,30 +10,37 @@ namespace StringORM
     public class Tests
     {
         #region ReaderTests
+
         [Fact]
         public void get_reader_isnt_null_with_default_connection()
         {
             SqlDataReader reader = "SELECT * FROM dbo.Names".GetDataReader();
             reader.Should().NotBeNull();
         }
+
         [Fact]
         public void get_reader_isnt_null_with_specific_connection()
         {
             SqlDataReader reader = "SELECT * FROM dbo.Names".GetDataReader(DataBase.Default);
             reader.Should().NotBeNull();
         }
+
         [Fact]
         public void get_reader_isnt_null_with_parameter_with_default_connection()
         {
-            SqlDataReader reader = "SELECT * FROM dbo.Names where Name = @Name".GetDataReader(new SqlParameter("@Name", "SomeNameWithId"));
+            SqlDataReader reader =
+                "SELECT * FROM dbo.Names where Name = @Name".GetDataReader(new SqlParameter("@Name", "SomeNameWithId"));
             reader.Should().NotBeNull();
         }
+
         [Fact]
         public void get_reader_isnt_null_with_parameter_with_specific_connection()
         {
-            SqlDataReader reader = "SELECT * FROM dbo.Names where Name = @Name".GetDataReader(DataBase.Default, new SqlParameter("@Name", "SomeNameWithId"));
+            SqlDataReader reader = "SELECT * FROM dbo.Names where Name = @Name".GetDataReader(DataBase.Default,
+                new SqlParameter("@Name", "SomeNameWithId"));
             reader.Should().NotBeNull();
         }
+
         [Fact]
         public void get_reader_isnt_null_with_list_of_parameters_with_default_connection()
         {
@@ -46,6 +52,7 @@ namespace StringORM
             SqlDataReader reader = "SELECT * FROM dbo.Names where Name = @Name AND Id = @Id".GetDataReader(sqlParameters);
             reader.Should().NotBeNull();
         }
+
         [Fact]
         public void get_reader_isnt_null_with_list_of_parameters_with_specific_connection()
         {
@@ -54,36 +61,45 @@ namespace StringORM
                 new SqlParameter("@Name", "SomeNameWithId"),
                 new SqlParameter("@Id", 1)
             };
-            SqlDataReader reader = "SELECT * FROM dbo.Names where Name = @Name AND Id = @Id".GetDataReader(DataBase.Default, sqlParameters);
+            SqlDataReader reader =
+                "SELECT * FROM dbo.Names where Name = @Name AND Id = @Id".GetDataReader(DataBase.Default, sqlParameters);
             reader.Should().NotBeNull();
         }
+
         #endregion
 
         #region DataTableTests
+
         [Fact]
         public void get_data_table_isnt_null_with_default_connection()
         {
             DataTable table = "SELECT * FROM dbo.Names".GetDataTable();
             table.Should().NotBeNull();
         }
+
         [Fact]
         public void get_data_table_isnt_null_with_specific_connection()
         {
             DataTable table = "SELECT * FROM dbo.Names".GetDataTable(DataBase.Default);
             table.Should().NotBeNull();
         }
+
         [Fact]
         public void get_data_table_isnt_null_with_parameter_with_default_connection()
         {
-            DataTable table = "SELECT * FROM dbo.Names where Name = @Name".GetDataTable(new SqlParameter("@Name", "SomeParamName"));
+            DataTable table =
+                "SELECT * FROM dbo.Names where Name = @Name".GetDataTable(new SqlParameter("@Name", "SomeParamName"));
             table.Should().NotBeNull();
         }
+
         [Fact]
         public void get_data_table_isnt_null_with_parameter_with_specific_connection()
         {
-            DataTable table = "SELECT * FROM dbo.Names where Name = @Name".GetDataTable(DataBase.Default, new SqlParameter("@Name", "SomeParamName"));
+            DataTable table = "SELECT * FROM dbo.Names where Name = @Name".GetDataTable(DataBase.Default,
+                new SqlParameter("@Name", "SomeParamName"));
             table.Should().NotBeNull();
         }
+
         [Fact]
         public void get_data_table_isnt_null_with_list_of_parameters_with_default_connection()
         {
@@ -95,6 +111,7 @@ namespace StringORM
             DataTable table = "SELECT * FROM dbo.Names where Name = @Name".GetDataTable(sqlParameters);
             table.Should().NotBeNull();
         }
+
         [Fact]
         public void get_data_table_isnt_null_with_list_of_parameters_with_specific_connection()
         {
@@ -106,33 +123,41 @@ namespace StringORM
             DataTable table = "SELECT * FROM dbo.Names where Name = @Name".GetDataTable(DataBase.Default, sqlParameters);
             table.Should().NotBeNull();
         }
+
         #endregion
 
         #region ScalarTests
+
         [Fact]
         public void get_scalar_isnt_null_with_default_connection()
         {
             object scalar = "SELECT * FROM dbo.Names".GetScalar();
             scalar.Should().NotBeNull();
         }
+
         [Fact]
         public void get_scalar_isnt_null_with_specific_connection()
         {
             object scalar = "SELECT * FROM dbo.Names".GetScalar(DataBase.Default);
             scalar.Should().NotBeNull();
         }
+
         [Fact]
         public void get_scalar_isnt_null_with_parameter_with_default_connection()
         {
-            object scalar = "SELECT * FROM dbo.Names where Name = @Name".GetScalar(new SqlParameter("@Name", "SomeParamName"));
+            object scalar =
+                "SELECT * FROM dbo.Names where Name = @Name".GetScalar(new SqlParameter("@Name", "SomeParamName"));
             scalar.Should().NotBeNull();
         }
+
         [Fact]
         public void get_scalar_isnt_null_with_parameter_with_specific_connection()
         {
-            object scalar = "SELECT * FROM dbo.Names where Name = @Name".GetScalar(DataBase.Default, new SqlParameter("@Name", "SomeParamName"));
+            object scalar = "SELECT * FROM dbo.Names where Name = @Name".GetScalar(DataBase.Default,
+                new SqlParameter("@Name", "SomeParamName"));
             scalar.Should().NotBeNull();
         }
+
         [Fact]
         public void get_scalar_isnt_null_with_list_of_parameters_with_default_connection()
         {
@@ -144,6 +169,7 @@ namespace StringORM
             object scalar = "SELECT * FROM dbo.Names where Name = @Name and Id = @Id".GetScalar(sqlParameters);
             scalar.Should().NotBeNull();
         }
+
         [Fact]
         public void get_scalar_isnt_null_with_list_of_parameters_with_specific_connection()
         {
@@ -152,34 +178,42 @@ namespace StringORM
                 new SqlParameter("@Name", "SomeNameWithId"),
                 new SqlParameter("@Id", 1)
             };
-            object scalar = "SELECT * FROM dbo.Names where Name = @Name and Id = @Id".GetScalar(DataBase.Default, sqlParameters);
+            object scalar = "SELECT * FROM dbo.Names where Name = @Name and Id = @Id".GetScalar(DataBase.Default,
+                sqlParameters);
             scalar.Should().NotBeNull();
         }
+
         #endregion
 
         #region ExecuteTests
+
         [Fact]
         public void can_execute_without_exception_with_default_connection()
         {
             @"INSERT INTO Names (Name)
             VALUES ('SomeName')".Execute();
         }
+
         [Fact]
         public void can_execute_without_exception_with_specific_connection()
         {
             @"INSERT INTO Names (Name)
             VALUES ('SomeName')".Execute(DataBase.Default);
         }
+
         [Fact]
         public void can_execute_without_exception_using_parameters_with_default_connection()
         {
             "INSERT INTO Names (Name) VALUES (@Name)".Execute(new SqlParameter("@Name", "SomeParamName"));
         }
+
         [Fact]
         public void can_execute_without_exception_using_parameters_with_specific_connection()
         {
-            "INSERT INTO Names (Name) VALUES (@Name)".Execute(DataBase.Default, new SqlParameter("@Name", "SomeParamName"));
+            "INSERT INTO Names (Name) VALUES (@Name)".Execute(DataBase.Default,
+                new SqlParameter("@Name", "SomeParamName"));
         }
+
         [Fact]
         public void can_execute_without_exception_using_list_of_parameters_with_default_connection()
         {
@@ -190,6 +224,7 @@ namespace StringORM
             };
             "INSERT INTO Names (Name, Id) VALUES (@Name, @Id)".Execute(sqlParameters);
         }
+
         [Fact]
         public void can_execute_without_exception_using_list_of_parameters_with_specific_connection()
         {
@@ -200,6 +235,7 @@ namespace StringORM
             };
             "INSERT INTO Names (Name, Id) VALUES (@Name, @Id)".Execute(DataBase.Default, sqlParameters);
         }
+
         #endregion
     }
 }
